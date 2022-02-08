945634c2bb4822745774780136625baa231acdae
swatigupta121 <gptswati121@gmail.com>
2022-02-08 20:15:33 +0530

    added login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./components/home/home";
+import Login from "./components/login/login";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Login />
     </div>
   );
 }
