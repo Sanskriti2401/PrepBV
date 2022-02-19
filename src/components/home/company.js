68ee51cbbf661f46b5d10a0faedb96c9969a2b94
sanskritinegi2401@gmail.com <sanskritinegi2401@gmail.com>
2022-02-19 16:59:12 +0530

    home

---
+++ src/components/home/company.js
@@ -0,0 +1,42 @@
+/*import a from "../../images/gs.png";
+import b from "../../images/flip.jpeg";
+import c from "../../images/amex.png";
+import d from "../../images/o.png";
+import e from "../../images/cisco.jpg";
+import f from "../../images/bar.jpg";*/
+
+const company = [
+  {
+    id: 1,
+    name: "Goldman Sachs",
+    url: "https://pbs.twimg.com/profile_images/1286311065966579713/JNU4bWC8_400x400.png",
+  },
+
+  {
+    id: 2,
+    name: "Flipkart",
+    url: "https://media-exp1.licdn.com/dms/image/C560BAQF6H8gAs-JyFg/company-logo_200_200/0/1627543110554?e=2159024400&v=beta&t=s136zw_Al97i6X22HhDewS48cRKvfSVzGbWEO0ZaQd0",
+  },
+  {
+    id: 3,
+    name: "American Express",
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/American_Express_logo_%282018%29.svg/1200px-American_Express_logo_%282018%29.svg.png",
+  },
+  {
+    id: 4,
+    name: "Oracle",
+    url: "https://pbs.twimg.com/profile_images/1448975437246255105/cc989Pmx_400x400.jpg",
+  },
+  {
+    id: 5,
+    name: "Cisco",
+    url: "https://media-exp1.licdn.com/dms/image/C560BAQH1e-s-hTeQ_w/company-logo_200_200/0/1634307374798?e=2159024400&v=beta&t=m4uvrn12CJXzkY_-NZHy4aA34nDdI31nVdQct0u6bbM",
+  },
+  {
+    id: 6,
+    name: "Barclays",
+    url: "https://www.directorstalkinterviews.com/wp-content/uploads/2017/01/Barclays.png",
+  },
+];
+
+export default company;
