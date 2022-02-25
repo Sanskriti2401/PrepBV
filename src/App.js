b6a1b89c671f0abfc4809f6a4afa37d8cf39f9c1
Sanskriti Negi <sanskritinegi2401@gmail.com>
2022-02-25 15:08:32 +0530

    Revert "Added Connect With Senior Component and other required changes in login component"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/login/login";
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Home />
     </div>
   );
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -5,29 +5,28 @@ import logo from "../../images/logo.jpeg";
 import LoginBasic from "../login_basic/login_basic";
 
 export default function Login() {
-    return (
-        <>
-            <div className="login">
-                <div className="login-left-part">
-                    <div className="login-bg"></div>
-                    <div className="login-img">
-                        <img src={img1} alt="Error" />
-                    </div>
-                    <div className="login-logo">
-                        <img src={logo} alt="PrepBV" />
-                    </div>
-                </div>
-                <div className="login-right-part">
-                    <div className="login-text">
-                        <h3>Login</h3>
-                        <p className="login-user">New User?</p>
-                        <p className="login-signup">Sign Up</p>
-                        <hr className="login-hr"></hr>
-                        <LoginBasic />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="login">
+        <div className="left-part">
+          <div className="bg"></div>
+          <div className="img">
+            <img src={img1} alt="Error" />
+          </div>
+          <div className="logo">
+            <img src={logo} alt="PrepBV" />
+          </div>
+        </div>
+        <div className="right-part">
+          <div className="text">
+            <h3>Login</h3>
+            <p className="user">New User?</p>
+            <p className="signup">Sign Up</p>
+            <hr className="hr"></hr>
+            <LoginBasic />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
