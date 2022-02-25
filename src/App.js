011e5ef53a7ee4a4911b7d678017dc4c668cce1a
swatigupta121 <gptswati121@gmail.com>
2022-02-25 20:21:04 +0530

    Added connect with senior component and other changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/login/login";
+import Connect from "./components/ConnectWithSenior/connect";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Connect />
     </div>
   );
 }
