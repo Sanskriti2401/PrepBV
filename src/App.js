71639b7bcbf200d167cd45496e3931d82893cbe8
sanskritinegi2401@gmail.com <sanskritinegi2401@gmail.com>
2022-02-27 00:49:01 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/login/login";
-import Practice from "./components/practice/practice";
-import topicDetails from "./components/practice/topicDetails";
+import Practice from "./components/Practice/practice";
+import topicDetails from "./components/Practice/topicDetails";
 import Nav from "./components/nav/nav";
-function createCard(topicDetails)
-{
-  return  <>
-  <Practice 
-  key={topicDetails.id}
-  TopicName={topicDetails.topicName}
-  QCount={topicDetails.qCount}
-  percentdone={topicDetails.percentdone}
-  />
-  </>
+function createCard(topicDetails) {
+  return (
+    <>
+      <Practice
+        key={topicDetails.id}
+        TopicName={topicDetails.topicName}
+        QCount={topicDetails.qCount}
+        percentdone={topicDetails.percentdone}
+      />
+    </>
+  );
 }
 function App() {
   return (
     <div className="App">
-       <Nav/>
+      <Nav />
       {topicDetails.map(createCard)}
     </div>
   );
