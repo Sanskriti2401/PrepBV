d056833ec1d696839be726b1e84bb6114706a2f9
sanskritinegi2401@gmail.com <sanskritinegi2401@gmail.com>
2022-02-27 17:37:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/login/login";
 import Connect from "./components/ConnectWithSenior/connect";
-import Practice from "./components/practice/practice";
-import topicDetails from "./components/practice/topicDetails";
-import Nav from "./components/nav/nav";
-function createCard(topicDetails) {
-  return (
-    <>
-      <Practice
-        key={topicDetails.id}
-        TopicName={topicDetails.topicName}
-        QCount={topicDetails.qCount}
-        percentdone={topicDetails.percentdone}
-      />
-    </>
-  );
-}
+import Prac from "./components/practice/prac";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      {topicDetails.map(createCard)}
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/practice" element={<Prac />} />
+          <Route exact path="/connect" element={<Connect />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/connect" element={<Connect />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 export default App;
+
+/*
+<Route path="/home">
+{login ? <Profile/>: <Redirect to="/" />} 
+</Route>
+
+
+*/
