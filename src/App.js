646ae76aa024c58f0e068dd471712f5e7b8eab17
Saumya <saumyasingh1508@gmail.com>
2022-02-27 17:22:37 +0530

    Sign up page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Login from "./components/login/login";
+/*import Login from "./components/login/login";*/
+/*import { BrowserRouter as Router,Switch, Route } from "react-router-dom";*/
+import EditProfile from "./components/editProfile/ProfileScreen"
+import SignupPage from "./components/signup/SignupPage";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <SignupPage />
+      
     </div>
   );
 }
