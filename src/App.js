320bee913c48616cc05ce83af351cd270f8ea153
Saumya <saumyasingh1508@gmail.com>
2022-02-27 19:36:31 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/login/login";
+import Connect from "./components/ConnectWithSenior/connect";
+import Prac from "./components/practice/prac";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import EditProfile from "./components/editProfile/ProfileScreen"
-import SignupPage from "./components/signup/SignupPage";
 
 function App() {
-    return (
-      <Router>
-        <div>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
-
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/practice" element={<Prac />} />
+          <Route exact path="/connect" element={<Connect />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/connect" element={<Connect />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 export default App;
+
+/*
+<Route path="/home">
+{login ? <Profile/>: <Redirect to="/" />} 
+</Route>
+
+
+*/
