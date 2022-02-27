fd72f6e58f083c569407b2939ad764cedaad4d4f
Saumya <saumyasingh1508@gmail.com>
2022-02-27 19:33:21 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/login/login";
-/*import { BrowserRouter as Router,Switch, Route } from "react-router-dom";*/
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EditProfile from "./components/editProfile/ProfileScreen"
 import SignupPage from "./components/signup/SignupPage";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      
-    </div>
-  );
-}
+    return (
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 
 export default App;
