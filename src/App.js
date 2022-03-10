3afe10d4eb858f6ea7a34e23a08b8ab99cc64fe4
SnehaAgarwal361 <snehaagarwalbly361@gmail.com>
2022-03-10 19:11:06 +0530

    app file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./App.css";
 import Home from "./components/home/home";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./components/login/login";
@@ -5,7 +6,12 @@ import Connect from "./components/ConnectWithSenior/connect";
 import Prac from "./components/practice/prac";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUpPage from "./components/signup/SignupPage";
-//import Gs from "./components/home/company/gs";
+import Gs from "./components/home/company/gs";
+import Flip from "./components/home/company/flip";
+import Amex from "./components/home/company/amex";
+import Cisco from "./components/home/company/cisco";
+import Oracle from "./components/home/company/oracle";
+import Barclays from "./components/home/company/barclays";
 
 function App() {
   return (
@@ -15,11 +21,14 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/practice" element={<Prac />} />
           <Route exact path="/connect" element={<Connect />} />
-          <Route exact path="/login" element={<Login />} />
-          {/*<Route exact path="/gs" element={<Gs />} />*/}
-          <Route exact path="/flip" element={<SignUpPage />} />
-          <Route exact path="/barclays" element={<SignUpPage />} />
           <Route exact path="/signup" element={<SignUpPage />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/gs" element={<Gs />} />
+          <Route exact path="/flip" element={<Flip />} />
+          <Route exact path="/amex" element={<Amex />} />
+          <Route exact path="/cisco" element={<Cisco />} />
+          <Route exact path="/oracle" element={<Oracle />} />
+          <Route exact path="/barclays" element={<Barclays />} />
         </Routes>
       </div>
     </Router>
