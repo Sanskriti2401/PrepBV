433357a632462021df1fb487b73fc04ba9a98206
swatigupta121 <gptswati121@gmail.com>
2022-04-06 09:52:00 +0530

    Added initial changes

--- Frontend/src/components/signup/FormSuccess.js
+++ Frontend/src/components/signup/FormSuccess.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 const FormSuccess = () => {
   return (
+    <>
     <div className="form-content-right">
         <div className="form-success">You are now regsitered!</div>
         <img src="https://c.tenor.com/IAUvvrUY7zQAAAAM/done-spongebob.gif" alt="success img" className="form-img-3" />
+        <div className='success-btn'>
+      <a href='./'><button className='success-button'>Login Here</button></a>
     </div>
-
+    </div>
+    
+    </>
   )
 }
 
