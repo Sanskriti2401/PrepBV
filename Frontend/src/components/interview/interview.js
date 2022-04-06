2969a4557b674b64e8d2efa13b4532a5b0fe2c1a
sanskritinegi2401@gmail.com <sanskritinegi2401@gmail.com>
2022-04-07 02:06:36 +0530

    interview fetch

--- Frontend/src/components/interview/interview.js
+++ Frontend/src/components/interview/interview.js
@@ -2,61 +2,49 @@ import React from "react";
 import Nav from "../nav/nav";
 import "./interview.css";
 import { useState, useEffect } from "react";
-<>
-  <link
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-    rel="stylesheet"
-  />
-  <link
-    href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
-    rel="stylesheet"
-  />
-</>;
+import { GiMagnifyingGlass } from "react-icons/gi";
 
 export default function Interview() {
-  const [getData, getSetData] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [getData, getSetData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const fetchData = async () => {
-      try{
-        await fetch("http://localhost:8000/interview").then(res => res.json())
+    try {
+      await fetch("http://localhost:8000/interview")
+        .then((res) => res.json())
         .then((data) => {
-          const {body}=data
-          getSetData(body)
-      
-          console.log(body)
-          console.log("hello");
-          setLoading(false)
-        })
-      } catch(err){
-      console.log(err)
-      }
-  }
-  
+          console.log(data);
+          const { application } = data;
+          getSetData(application);
+
+          setLoading(false);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
-  fetchData()
-  }, [])
-  
+    fetchData();
+  }, []);
+
   return (
     <>
       <Nav />
       <div className="head-interview">
         <div className="heading-i">Interview Experience</div>
-        
+
         <div className="search">
           <input type="text" placeholder="Enter Company Name"></input>
-          <i className="fa fa-search"></i>
+          <GiMagnifyingGlass />
         </div>
       </div>
-    
+
       {getData.map((todo) => (
         <div className="question-info">
           <h1>{todo.title}</h1>
           <p>{todo.content}</p>
-          console.log(todo.title);
         </div>
       ))}
-
-      
     </>
   );
 }
