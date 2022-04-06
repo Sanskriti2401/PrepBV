f3a45f6dd09beba67c68e43597aa10dd65f4b95a
swatigupta121 <gptswati121@gmail.com>
2022-04-06 10:11:55 +0530

    added a small change

--- Frontend/src/components/nav/nav.js
+++ Frontend/src/components/nav/nav.js
@@ -12,7 +12,7 @@ export default function Nav() {
 
         <ul className="option">
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/home">Home</Link>
           </li>
           <li>
             <Link to="/practice">Practice questions</Link>
