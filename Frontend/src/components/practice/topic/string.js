74d22f3c37c5d89aedda0006bddc4bfeaf6e941d
swatigupta121 <gptswati121@gmail.com>
2022-04-25 21:21:36 +0530

    added bookmark function to all

--- Frontend/src/components/practice/topic/string.js
+++ Frontend/src/components/practice/topic/string.js
@@ -3,41 +3,74 @@ import { useState, useEffect } from "react";
 import Checkbox from "./Checkbox";
 import "./questions.css";
 import Nav from "../../nav/nav";
-  export default function Heap()
-   {
-   const [getData, getSetData] = useState([])
-    const [loading, setLoading] = useState(true)
-    const fetchData = async () => {
-        try{
-          await fetch("http://localhost:8000/QuestionSet/String").then(res => res.json())
-          .then((data) => {
-            const {requiredData}=data
-            getSetData(requiredData)
-            console.log(requiredData)
-            setLoading(false)
-          })
-        } catch(err){
-        console.log(err)
-        }
+import { BsBookHalf } from "react-icons/bs";
+
+const addit = (props) => {
+  const email = window.localStorage.getItem('user')
+  console.log(email);
+  console.log(props._id)
+  fetch("http://localhost:8000/Register", {
+    crossDomain: true,
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+
+    body: JSON.stringify({
+      id: props._id,
+      email: email
+    }),
+  }).then(function (response) {
+    console.log(response.body);
+
+    const { body } = response;
+
+    const { message } = body;
+
+    return response.json();
+  });
+  alert(`Added to you bookmark list!`)
+}
+
+export default function Heap() {
+  const [getData, getSetData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const fetchData = async () => {
+    try {
+      await fetch("http://localhost:8000/QuestionSet/String").then(res => res.json())
+        .then((data) => {
+          const { requiredData } = data
+          getSetData(requiredData)
+          console.log(requiredData)
+          setLoading(false)
+        })
+    } catch (err) {
+      console.log(err)
     }
-    
-    useEffect(() => {
-    fetchData()
-    }, [])
-    
-    return (
-        <div>
-          <Nav />
-        <div className="topic-heading">
-        <h1 >String</h1>
-        </div>
-        {getData.map((todo) => (
-          <div className="question-info">
-         <Checkbox />
-            <a href={todo.URL}>{todo.Problem}</a>
-          </div>
-        ))}
-        </div>
-    )
-    
   }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      <Nav />
+      <div className="topic-heading">
+        <h1 >String</h1>
+      </div>
+      {getData.map((todo) => (
+        <div className="question-info">
+          <Checkbox />
+          <a href={todo.URL}>{todo.Problem}</a>
+          <div className="book">
+            <span onClick={() => addit(todo)}>
+              <BsBookHalf />
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+
+}
